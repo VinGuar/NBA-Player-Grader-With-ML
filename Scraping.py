dc5d19cc1d793d108f33bf8b234b6f57958fb28b
VinGuar <126090788+VinGuar@users.noreply.github.com>
2023-03-09 17:56:44 +0000

    commit message

--- Scraping.py
+++ Scraping.py
@@ -8,4 +8,14 @@ names = player1.split()
 firstName = names[0]
 lastName = names[1]
 
+url = "https://www.basketball-reference.com/players/m/morrima01.html"
+
+index = url.find("players/") + 8
+
+url = url[]
+
+print(url[index:])
+
+
+
 
