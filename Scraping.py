338969b3c1622b81b2f573677d7dbc80506dd472
VinGuar <126090788+VinGuar@users.noreply.github.com>
2023-03-09 18:43:56 +0000

    joshes help

--- Scraping.py
+++ Scraping.py
@@ -8,13 +8,21 @@ names = player1.split()
 firstName = names[0]
 lastName = names[1]
 
-url = "https://www.basketball-reference.com/players/m/morrima01.html"
+url = "https://www.basketball-reference.com/players/"
+
 
 index = url.find("players/") + 8
 
-url = url[]
+url += (lastName[0] + "/")
+for num in range(5):
+    url += lastName[num]
+for num in range (2):
+    url += firstName[num]
 
-print(url[index:])
+url += "01.html"
+
+print(url)
+
 
 
 
