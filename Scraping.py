bf0659e445382e7da8268fc74dbc67d2a50f730d
VinGuar <126090788+VinGuar@users.noreply.github.com>
2023-03-09 05:07:21 +0000

    3/9/23 work

--- Scraping.py
+++ Scraping.py
@@ -1,1 +1,11 @@
-print(1)
+import UserInput
+
+#player name from User
+player1 = UserInput.player
+
+#creating first and last name variables for player
+names = player1.split()
+firstName = names[0]
+lastName = names[1]
+
+
--- UserInput.py
+++ UserInput.py
@@ -1,1 +1,37 @@
-print(1)
+import PlayerRanking
+import TeamRankings
+import GameSimulations
+
+#deciding which part of program to utilize
+print("Which of the following options do you want?")
+print("")
+print("1: A particular NBA player's statistics and grade for a given year")
+print("2: A list, up to a number of your choosing, of NBA players ranked in the current year")
+print("3: A list, up to a number of your choosing, of NBA teams ranked in the current year")
+print("4: A percentage predictor of a game between two particular teams")
+print("")
+
+choice = input("Please select a number 1-4 (no spaces) for these options: ")
+
+#making sure choice is a number 1-4
+while True:
+    try:
+        int(choice)
+        if int(choice) in (1,2,3,4):
+            break
+        else:
+            choice = input("Please enter a number digit 1-4 (no spaces): ")
+    except:
+        choice = input("Please enter a number digit 1-4 (no spaces): ")
+
+#making choice an int
+choice = int(choice)
+
+
+if (choice == 1):
+    player = input("Please enter a players exact first and last name: ")
+
+
+
+
+    
