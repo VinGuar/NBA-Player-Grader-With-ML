074008d67e084d08e644c562a95b0491f9145936
VinGuar <126090788+VinGuar@users.noreply.github.com>
2023-03-12 03:44:49 +0000

    maybeitllwork

--- Scraping.py
+++ Scraping.py
@@ -1,9 +1,8 @@
 from sportsipy.nba.roster import Player
+from sportsipy.nba.roster import Roster
 
-james_harden = Player('hardeja01')
-print(james_harden.name)  # Prints 'James Harden'
-print(james_harden.points)  # Prints Harden's career points total
-# Prints a Pandas DataFrame of all relevant Harden stats per season
-print(james_harden.dataframe)
-print(james_harden.salary)  # Prints Harden's career earnings
-print(james_harden.contract)  # Prints Harden's contract by yearly wages
+houston = Roster('BRK')
+for player in houston.players:
+    # Prints the name of all players who played for Houston in the most
+    # recent season.
+    print(player.name)
