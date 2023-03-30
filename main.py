5cced5e8c72dfdb266a1bcd353bf395fcd84fdcd
VinGuar <126090788+VinGuar@users.noreply.github.com>
2023-03-30 03:50:00 +0000

    first

--- main.py
+++ main.py
@@ -5,69 +5,6 @@ import TeamRankings
 import UserInput
 import PlayerRanking
 
-"""
-The code below was originally going to be used to make a team-based grade using player stats, 
-however for purpose of program I am commenting it out.
-
-def makeTeamDict(rosterArr):
-    dict = {}
-    for n in range(len(rosterArr)):
-        name = rosterArr[n]
-
-        url = Scraping.scrapeURL(name, "2022-23")
-        stats = Scraping.scrapeStats(url, "2022-23")
-        if len(stats)<8:
-            continue
-        else:
-            dict[name] = stats
-    return dict
-
-def giveGrade(rosterDict):
-    sortedDict = dict(sorted(rosterDict.items(), key=lambda item: float(item[1]['MP']), reverse=True))
-    x = 0
-    totalMin = 0
-    teamGrade = 0
-    for key in sortedDict:
-        if x > 9:
-            break
-        if key == "LaMelo Ball" or key == "Terry Rozier" or key == "Kelly Oubre Jr.":
-            continue
-
-        stats = sortedDict[key]
-        mp = stats["MP"]
-        mp = float(mp)
-        totalMin = totalMin + mp
-
-        statswmin2 = stats.copy()
-        
-        stats.pop("MP")
-
-        percentile = PlayerRanking.percentile(stats, 2023, key, False, False)
-        pos = PlayerRanking.percentile(stats, 2023, key, True, False)
-        grade = PlayerRanking.grader(percentile, pos, False)
-
-        #per36 = PlayerRanking.per36(statswmin2, 2023, key)
-        #print(grade, teamGrade, mp, totalMin)
-
-        if (totalMin + mp)>230:
-            teamGrade = teamGrade + (230-totalMin)*grade
-            totalMin = 230
-            break
-        else:
-            teamGrade = teamGrade + mp*grade
-
-        x+=1
-    print(totalMin)
-    teamGrade = teamGrade/totalMin
-    return teamGrade
-
-"""
-            
-
-
-
-
-
 choice = UserInput.inputNum()
 
 if choice == 1 or choice == 2 or choice == 3:
@@ -97,39 +34,3 @@ if choice == 1 or choice == 2 or choice == 3:
         print(f"Per 36 grade: {per36}")
     elif choice == 1:
         print(f"Regular grade: {grade}")
-        
-"""
-Same thing as the top, this code belowwas to make team grades.
-
-elif choice == 2:
-    teamOne = UserInput.teamOne()
-    teamTwo = UserInput.teamTwo()
-    status = True
-
-    rosterTeamOne = Scraping.scrapePlayers(status, teamOne)
-    rosterTeamTwo = Scraping.scrapePlayers(status, teamTwo)
-
-    #print(rosterTeamOne)
-    #print(rosterTeamTwo)
-    one = makeTeamDict(rosterTeamOne)
-    two = makeTeamDict(rosterTeamTwo)
-
-    oneGrade = giveGrade(one)
-    twoGrade = giveGrade(two)
-
-    #print(one)
-    print(f"{teamOne} grade is: {oneGrade}, and {teamTwo} grade is: {twoGrade}")
-    #print(two)
-
-"""
-
-
-
-
-            
-
-
-
-
-
-
